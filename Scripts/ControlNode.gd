5c936b7e3578b8d45661a6250b9318603101f866
Luke LaBonte <lukesterlab@gmail.com>
2022-01-30 23:50:03 -0500

    Beziers

--- Scripts/ControlNode.gd
+++ Scripts/ControlNode.gd
@@ -1,11 +1,13 @@
 extends Node2D
 
-onready var curve = Curve2D.new()
-
 var inputNodes = []
 var outputNodes = []
 var node
 var pos
+var halfwayPoints = []
+
+var IntersectionCreatorNode;
+var curveControlPoint = null;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,11 +19,51 @@ func addInputNode(n):
 func addOutputNode(n):
 	outputNodes.append(n)
 
+	halfwayPoints.append(Vector2((pos.x + outputNodes[0].pos.x) / 2, (pos.y + outputNodes[0].pos.y) / 2))
+	halfwayPoints[-1] = IntersectionCreatorNode.getGridPosition(halfwayPoints[-1])
+
+func setHalfwayPoint(p, index):
+	halfwayPoints[index] = p;
+
 func setNode(n):
 	node = n
 	pos = n.position
 func getNode():
 	return node
+func setIntersectionCreator(n):
+	IntersectionCreatorNode = n;
+
+func constructCurve():
+	var curve = Curve2D.new()
+	var points = getBezierPointsFromVecs(pos, halfwayPoints[-1], outputNodes[0].pos, .1)
+
+	#No way to just set the curve array to 'points', so we have to loop through
+	for p in points:
+		curve.add_point(p)
+
+	if(!curveControlPoint):
+		curveControlPoint = Sprite.new()
+		curveControlPoint.texture = IntersectionCreatorNode.HIGHLIGHTED_TEX
+		curveControlPoint.position = halfwayPoints[-1] 
+		curveControlPoint.scale = Vector2(.03, .03)
+		IntersectionCreatorNode.add_child(curveControlPoint)
+		IntersectionCreatorNode.controlPointsDict[halfwayPoints[-1]] = [self, len(halfwayPoints) - 1]
+
+	return curve
+
+func getBezierPointsFromVecs(a, b, c, smooth):
+	#Generate points on curve (see https://stackoverflow.com/questions/5634460/quadratic-b%C3%A9zier-curve-calculate-points)
+	var bezier = []
+
+	var i = 0
+	while i <= 1:
+		var x1 = (1 - i) * (1 - i) * a.x + 2 * (1 - i) * i * b.x + i * i * c.x
+		var y1 = (1 - i) * (1 - i) * a.y + 2 * (1 - i) * i * b.y + i * i * c.y
+
+		bezier.append(Vector2(x1, y1))
+
+		i += smooth
+	return bezier
 	
 func getLineCoords():
 	if len(outputNodes) > 0:
--- Scripts/IntersectionCreator.gd
+++ Scripts/IntersectionCreator.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
-var GRID_SPACING = 25;
+var GRID_SPACING = 25
+var DRAGGING_CTRL = false
+var CONTROL_POINT_INDEX = -1
+var ORIG_CTRL_POINT
 
 onready var HIGHLIGHTED_TEX = load("res://Images/Highlighted.png")
 onready var UNHIGHLIGHTED_TEX = load("res://Images/Unhighlighted.png")
@@ -9,70 +12,95 @@ onready var ControlNodes = load("res://Scripts/ControlNode.gd")
 var selectedNode = null
 
 var nodeDict = {}
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var controlPointsDict = {}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	if DRAGGING_CTRL:
+		selectedNode.curveControlPoint.position = getGridPosition(get_global_mouse_position())
+		selectedNode.setHalfwayPoint(getGridPosition(get_global_mouse_position()), CONTROL_POINT_INDEX)
+		update() 
 
 func _input(event):
 	if event is InputEventMouseButton && event.is_pressed() && event.button_index == BUTTON_LEFT:
-		Instantiate(get_global_mouse_position())
+		HandleLeftClick(get_global_mouse_position())
 		
 	if event is InputEventKey && event.scancode == KEY_ESCAPE:
 		if(selectedNode): selectedNode.getNode().texture = UNHIGHLIGHTED_TEX;
 		selectedNode = null
 
+	if event is InputEventMouseButton && event.is_pressed() == false && event.button_index == BUTTON_LEFT && DRAGGING_CTRL:
+		if selectedNode:
+			selectedNode.setHalfwayPoint(getGridPosition(get_global_mouse_position()), CONTROL_POINT_INDEX)
+			controlPointsDict[getGridPosition(get_global_mouse_position())] = [selectedNode, CONTROL_POINT_INDEX]
+			controlPointsDict.erase(ORIG_CTRL_POINT)
+			selectedNode = null
+			update()
+		DRAGGING_CTRL = false
 
-func Instantiate(position):
-	var gridPosition = Vector2(roundToNearest(position.x, GRID_SPACING), roundToNearest(position.y, GRID_SPACING));
+
+func HandleLeftClick(position):
+	var gridPosition = getGridPosition(position)
 
 	if(!nodeDict.has(gridPosition)):
-		var node = Sprite.new()
-		var control = ControlNodes.new()
+		if controlPointsDict.has(gridPosition):
+			if(selectedNode): selectedNode.getNode().texture = UNHIGHLIGHTED_TEX;
+			selectedNode = controlPointsDict[gridPosition][0]
+			CONTROL_POINT_INDEX = controlPointsDict[gridPosition][1]
+			DRAGGING_CTRL = true
+			ORIG_CTRL_POINT = gridPosition
 
-		node.texture = HIGHLIGHTED_TEX
-		node.position = gridPosition
-		node.scale = Vector2(.05, .05)
-		
-		control.setNode(node)
-
-		if selectedNode:
-			selectedNode.addOutputNode(control)
-			control.addInputNode(selectedNode)
-			selectedNode.getNode().texture = UNHIGHLIGHTED_TEX;
-			selectedNode = control;
-		else: selectedNode = control
-
-		add_child(node)
-		nodeDict[gridPosition] = control
+		else: spawnNewNode(gridPosition)
 
 	else:
-		if selectedNode: 
-			selectedNode.addOutputNode(nodeDict[gridPosition])
-			nodeDict[gridPosition].addInputNode(selectedNode)
-			selectedNode.getNode().texture = UNHIGHLIGHTED_TEX;
-			
-		selectedNode = nodeDict[gridPosition];
-		selectedNode.getNode().texture = HIGHLIGHTED_TEX;
+		switchFocusToNode(gridPosition)
 		
 	#CanvasItem.update, built in
 	update()
+
+func spawnNewNode(gridPosition):
+	var node = Sprite.new()
+	var control = ControlNodes.new()
+
+	node.texture = HIGHLIGHTED_TEX
+	node.position = gridPosition
+	node.scale = Vector2(.05, .05)
+
+	control.setNode(node)
+	control.setIntersectionCreator(self)
+
+	if selectedNode:
+		selectedNode.addOutputNode(control)
+		control.addInputNode(selectedNode)
+		selectedNode.getNode().texture = UNHIGHLIGHTED_TEX;
+		selectedNode = control;
+	else: selectedNode = control
+
+	add_child(node)
+	nodeDict[gridPosition] = control
 	
+
+func switchFocusToNode(gridPosition):
+	if selectedNode: 
+		selectedNode.addOutputNode(nodeDict[gridPosition])
+		nodeDict[gridPosition].addInputNode(selectedNode)
+		selectedNode.getNode().texture = UNHIGHLIGHTED_TEX;
+		
+	selectedNode = nodeDict[gridPosition];
+	selectedNode.getNode().texture = HIGHLIGHTED_TEX;
+
+func getGridPosition(position):
+	return Vector2(roundToNearest(position.x, GRID_SPACING), roundToNearest(position.y, GRID_SPACING));
+
 func roundToNearest(num, roundto):
 	return round(num / roundto) * roundto
 	
 	
 func _draw():
 	for n in nodeDict.values():
-		if(n.getLineCoords()):
-			for l in n.getLineCoords(): draw_line(l[0], l[1], Color(255, 255, 255))
+		if len(n.outputNodes) > 0:
+			draw_polyline(n.constructCurve().get_baked_points(), Color.red, 2.0)
